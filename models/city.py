7faac4586aa4624c093cfe3b953bdbff781723c9
root <5718@holbertonstudents.com>
2023-03-26 10:43:18 -0700

    Add relationship between user and place and city with place

--- models/city.py
+++ models/city.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """City class."""
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy import String, Column, Integer, ForeignKey
 
 
 class City(BaseModel, Base):
@@ -10,3 +10,5 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities',
+                          cascade='all, delete-orphan')
