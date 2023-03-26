905ecc16698b2961418324f4970b2cf8b8537b64
root <5718@holbertonstudents.com>
2023-03-26 12:12:16 -0700

    Fix error with import syntax

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchmey impoty String
+from sqlalchmey import String
+from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
