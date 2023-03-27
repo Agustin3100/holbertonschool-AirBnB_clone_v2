dabf525eeec0b801e79e0c336cc2b0245a8f0b52
root <5718@holbertonstudents.com>
2023-03-27 09:38:19 -0700

    Fix docstyle and pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+"""Hello FLask!."""
+
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
 
+@app.route('/', strict_slashes=False)
 def display(name=None):
+    """Return a string."""
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    
+
